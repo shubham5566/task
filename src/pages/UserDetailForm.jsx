45bfdf27082aa1fcd463cb19ab8732de006cdb31
shubham bhavsar <shubhambhavsar5566@gmail.com>
2024-10-25 19:18:25 +0530

    basic setUp is done

---
+++ src/pages/UserDetailForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserDetailForm = () => {
+  return (
+    <div>UserDetailForm</div>
+  )
+}
+
+export default UserDetailForm
---
+++ src/utils/validation.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const validation = () => {
+  return (
+    <div>validation</div>
+  )
+}
+
+export default validation
